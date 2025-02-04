dc603d153ea97b32ae00fd565087f01663a6ec50
啊汤哥 <atg_absorbed@163.com>
2025-02-04 15:15:08 +0800

    初始话项目

---
+++ src/main/java/com/atg/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.atg.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.atg.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
