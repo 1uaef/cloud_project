a747f47e6f51ffa6cdc15c28471db7c91f066ca9
啊汤哥 <atg_absorbed@163.com>
2025-03-17 18:38:55 +0800

    feat(database): 添加用户反馈表

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,8 +1,6 @@
 # 数据库初始化
 
 
-
-
 -- 切换库
 use cloud;
 
@@ -52,7 +50,6 @@ create table if not exists picture
 ) comment '图片' collate = utf8mb4_unicode_ci;
 
 
-
 -- 空间表
 create table if not exists space
 (
@@ -77,10 +74,10 @@ create table if not exists space
 
 ALTER TABLE picture
     -- 添加新列
-    ADD COLUMN reviewStatus INT DEFAULT 0 NOT NULL COMMENT '审核状态：0-待审核; 1-通过; 2-拒绝',
-    ADD COLUMN reviewMessage VARCHAR(512) NULL COMMENT '审核信息',
-    ADD COLUMN reviewerId BIGINT NULL COMMENT '审核人 ID',
-    ADD COLUMN reviewTime DATETIME NULL COMMENT '审核时间';
+    ADD COLUMN reviewStatus  INT DEFAULT 0 NOT NULL COMMENT '审核状态：0-待审核; 1-通过; 2-拒绝',
+    ADD COLUMN reviewMessage VARCHAR(512)  NULL COMMENT '审核信息',
+    ADD COLUMN reviewerId    BIGINT        NULL COMMENT '审核人 ID',
+    ADD COLUMN reviewTime    DATETIME      NULL COMMENT '审核时间';
 
 -- 创建基于 reviewStatus 列的索引
 CREATE INDEX idx_reviewStatus ON picture (reviewStatus);
@@ -92,7 +89,7 @@ ALTER TABLE picture
 
 -- 添加新列
 ALTER TABLE picture
-    ADD COLUMN spaceId  bigint  null comment '空间 id（为空表示公共空间）';
+    ADD COLUMN spaceId bigint null comment '空间 id（为空表示公共空间）';
 
 -- 创建索引
 CREATE INDEX idx_spaceId ON picture (spaceId);
@@ -105,7 +102,7 @@ CREATE INDEX idx_spaceType ON space (spaceType);
 
 
 
-
+use cloud;
 
 -- 空间成员表
 create table if not exists space_user
@@ -123,18 +120,120 @@ create table if not exists space_user
 ) comment '空间用户关联' collate = utf8mb4_unicode_ci;
 
 
-
-
-
-
-
 -- 功能扩展：用户个人主页资料修改
 ALTER TABLE user
-    ADD phoneNumber VARCHAR(20) COMMENT '手机号',
-    ADD email VARCHAR(256) COMMENT '邮箱',
-    ADD grade VARCHAR(50) COMMENT '年级',
-    ADD workExperience VARCHAR(512) COMMENT '工作经验',
+    ADD phoneNumber        VARCHAR(20) COMMENT '手机号',
+    ADD email              VARCHAR(256) COMMENT '邮箱',
+    ADD grade              VARCHAR(50) COMMENT '年级',
+    ADD workExperience     VARCHAR(512) COMMENT '工作经验',
     ADD expertiseDirection VARCHAR(512) COMMENT '擅长方向';
+
+
+
+use cloud;
+
+
+-- 题库表
+create table if not exists question_bank
+(
+    id          bigint auto_increment comment 'id' primary key,
+    title       varchar(256)                       null comment '标题',
+    description text                               null comment '描述',
+    picture     varchar(2048)                      null comment '图片',
+    userId      bigint                             not null comment '创建用户 id',
+    editTime    datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除',
+    index idx_title (title)
+) comment '题库' collate = utf8mb4_unicode_ci;
+
+
+-- 题目表
+create table if not exists question
+(
+    id         bigint auto_increment comment 'id' primary key,
+    title      varchar(256)                       null comment '标题',
+    tags       varchar(1024)                      null comment '标签列表（json 数组）',
+    answer     text                               null comment '推荐答案',
+    userId     bigint                             not null comment '创建用户 id',
+    editTime   datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除',
+    index idx_title (title),
+    index idx_userId (userId)
+) comment '题目' collate = utf8mb4_unicode_ci;
+
+-- 题库题目关联表
+
+use cloud;
+
+create table if not exists question_bank_question
+(
+    id             bigint auto_increment comment 'id' primary key,
+    questionBankId bigint                             not null comment '题库 id',
+    questionId     bigint                             not null comment '题目 id',
+    userId         bigint                             not null comment '创建用户 id',
+    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    UNIQUE (questionBankId, questionId)
+) comment '题库题目' collate = utf8mb4_unicode_ci;
+
+
+
+-- AI 模拟面试功能
+-- 模拟面试表
+create table if not exists mock_interview
+(
+    id             bigint auto_increment comment 'id' primary key,
+    workExperience varchar(256)                       not null comment '工作年限',
+    jobPosition    varchar(256)                       not null comment '工作岗位',
+    difficulty     varchar(50)                        not null comment '面试难度',
+    messages       mediumtext                         null comment '消息列表（JSON 对象数组字段，同时包括了总结）',
+    status         int      default 0                 not null comment '状态（0-待开始、1-进行中、2-已结束）',
+    userId         bigint                             not null comment '创建人（用户 id）',
+    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete       tinyint  default 0                 not null comment '是否删除（逻辑删除）',
+    index idx_userId (userId)
+) comment '模拟面试' collate = utf8mb4_unicode_ci;
+
+use cloud;
+
+-- 创建 feedback 表来存储反馈建议信息
+CREATE TABLE IF NOT EXISTS feedback
+(
+    -- 建议 ID，作为主键，自增整数类型
+    id          bigint AUTO_INCREMENT PRIMARY KEY COMMENT '建议 ID',
+    -- 提交建议的用户 ID，作为外键，关联到用户表（假设用户表名为 users，用户 ID 字段为 id）
+    userId      bigint NOT NULL COMMENT '用户 ID',
+    -- 建议详细描述，使用 TEXT 类型存储较长的文本内容
+    description TEXT NOT NULL COMMENT '建议详细描述',
+    -- 建议状态，整数类型，默认为 0 表示正在审核
+    status      INT       DEFAULT 0 COMMENT '建议状态(0-正在审核、1-审核通过、2-审核不通过)',
+    -- 建议创建时间，使用 TIMESTAMP 类型自动记录创建时的时间
+    createTime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null COMMENT '创建时间',
+    -- 建议更新时间，使用 TIMESTAMP 类型，在数据更新时自动更新时间
+    updateTime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null COMMENT '更新时间',
+    -- 是否删除标记，使用 TINYINT 类型，0 表示未删除，1 表示已删除
+    isDelete    TINYINT   DEFAULT 0 not null COMMENT '是否删除',
+    index  idx_feedback (id)
+) comment '反馈建议' collate = utf8mb4_unicode_ci;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
